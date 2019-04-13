e19cd17d2770a3ad510c2aa5bfe8b5ec13407a6d
Purushottam Shukla <formalspace@gmail.com>
2019-04-13 16:20:43 +0530

    Basic UI part 1

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("it works")
+ document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            plugins: [ 'dayGrid', 'interaction' ],
+        
+            events: [
+                {
+                    title: 'Day1',
+                    start: '2019-04-15'
+                },
+                {
+                    title: 'Day2',
+                    start: '2019-04-08'
+                }
+            ]
+            
+        });
+
+        calendar.render();
+        calendar.on('dateClick', function(info) {
+        console.log('clicked on ' + info.dateStr);
+});
+      });
