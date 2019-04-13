4e4ce764629b1c1b4020d8e9453e87ee1e7852dc
Purushottam Shukla <formalspace@gmail.com>
2019-04-14 01:32:45 +0530

    Price fetch from the Airtable

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-console.log("it works")
- document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
 
         var calendar = new FullCalendar.Calendar(calendarEl, {
             plugins: [ 'dayGrid', 'interaction' ],
-        
-            events: [
-                {
-                    title: 'Day1',
-                    start: '2019-04-15'
-                },
-                {
-                    title: 'Day2',
-                    start: '2019-04-08'
-                }
-            ]
-            
+            events: function(info, successCallback, failureCallback) {
+                let url = 'https://api.airtable.com/v0/applvIErrK8lnXyJK/Table%201?api_key=keydF5mdPvdf8vCa4';
+                fetch(url).then(res => res.json())
+                .then((out) => {
+                    console.log(typeof out.records)
+                    successCallback(
+                    Array.prototype.slice.call(out.records).map(function(eventEl) {
+                      return {
+                        id: eventEl.id,
+                        title: eventEl.fields.price,
+                        start: eventEl.fields.date
+                      }
+                    })
+                  )
+                })
+                
+            }
+   
         });
 
         calendar.render();
         calendar.on('dateClick', function(info) {
         console.log('clicked on ' + info.dateStr);
 });
+    event
       });
